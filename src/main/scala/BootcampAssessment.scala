87e4ac3190cddcd824471844563bdf6f1a7d01d4
Mathijs Hoogland <mathijs@hoogland.it>
2019-03-07 12:22:14 +0100

    Project init.

---
+++ src/main/scala/BootcampAssessment.scala
@@ -0,0 +1,38 @@
+object BootcampAssessment {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+
+  def uploadFiles(): Unit = {
+    //TODO: implement!
+  }
+
+  def createTables(): Unit = {
+    //TODO: implement!
+  }
+
+  def outputAggregatedInformation(): Unit = {
+    //TODO: implement!
+  }
+
+  def createHBaseTable(): Unit = {
+    //TODO: implement!
+  }
+
+  def loadCSV(): Unit = {
+    //TODO: implement!
+  }
+
+  def addFourthElement(): Unit = {
+    //TODO: implement!
+  }
+
+  def updateFourthElement(): Unit = {
+    //TODO: implement!
+  }
+
+  def outputInformation(): Unit = {
+    //TODO: implement!
+  }
+
+}
